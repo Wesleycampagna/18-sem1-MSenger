8cbcf63ccd4cc064ee0254b5081b8beed70076de
Wesley Campagna <wesley.campagna@hotmail.com>
2018-05-24 13:06:19 -0400

    inicio

---
+++ app/src/main/java/wesleyhelitonealberto/app/mensseger/messengerufms/ChatScreen.java
@@ -0,0 +1,13 @@
+package wesleyhelitonealberto.app.mensseger.messengerufms;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ChatScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat_screen);
+    }
+}
